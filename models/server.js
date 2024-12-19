a74f44012da6dd729f247f04cf058d9e9e35331b
Francisco Avalos <fjavpra@gmail.com>
2024-12-18 17:11:44 -0700

    add config cors

--- models/server.js
+++ models/server.js
@@ -2,12 +2,16 @@ const express = require("express");
 const mysql  = require("mysql2/promise");
 const { createServer } = require('http');
 const { db } = require('../database/config');
+const cors = require('cors')
+
 
 class Server {
     constructor(){
       this.app = express();
       this.port = process.env.PORT;
       this.server = createServer( this.app );
+      /* cors */
+      this.app.use(cors())
       this.app.use(express.json())
       this.paths = {
           units: '/api/units'
